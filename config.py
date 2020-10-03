aa41223cb374a7671231aae5cdac8414bb144e52
Will Dahl <wdahl@albany.edu>
2020-10-03 15:05:28 -0400

    data config script

---
+++ config.py
@@ -0,0 +1,42 @@
+import csv
+
+with open('data.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    KC_set = set()
+    for row in reader:
+        kc = row['KC (Original)']
+        KC_set.add(kc)
+    
+    KC_list = list(KC_set)
+
+    f.seek(0)
+
+    tensor = list()
+    matrix = list()
+    y = list()
+    current_std = ''
+    first = True
+    for row in reader:
+        std = row['Anon Student Id']
+
+        if first:
+            current_std = std
+            first = False
+
+        elif current_std != std:
+            tensor.append(matrix)
+            matrix = list()
+            current_std = std
+        
+        vector = [0 for i in range(len(KC_list))]
+        for i in range(len(KC_list)):
+            if row['KC (Original)'] == KC_list[i]:
+                vector[i] = 1
+                break
+
+        matrix.append(vector)
+
+        if row['outcome'] == 'CORRECT':
+            y.append(1)
+        else:
+            y.append(0)
